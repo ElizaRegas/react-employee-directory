89382c866ffa09050559f9804b4344154e0986d7
Eliza Regas <elizaregas@gmail.com>
2020-08-01 20:39:28 -0400

    Updates

---
+++ precache-manifest.18e6ee100b085203b19570a1a158fd94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e9cddeaacf38811894ec335b4ec871b",
+    "url": "/react-employee-directory/index.html"
+  },
+  {
+    "revision": "0884cc372127c7480ca2",
+    "url": "/react-employee-directory/static/css/main.b355e71c.chunk.css"
+  },
+  {
+    "revision": "9f94458d6d4eb8537ae7",
+    "url": "/react-employee-directory/static/js/2.e662b8ed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-employee-directory/static/js/2.e662b8ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0884cc372127c7480ca2",
+    "url": "/react-employee-directory/static/js/main.15a85383.chunk.js"
+  },
+  {
+    "revision": "193cdcbd01f85fb428f8",
+    "url": "/react-employee-directory/static/js/runtime-main.76415422.js"
+  }
+]);
