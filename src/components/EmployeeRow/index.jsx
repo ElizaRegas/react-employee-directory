1039d5f9cc6155de2e01f64209a04f92b1786bba
Eliza Regas <elizaregas@gmail.com>
2020-08-02 11:41:36 -0400

    profiles spaced well on page

--- src/components/EmployeeRow/index.jsx
+++ src/components/EmployeeRow/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
+import "./style.css";
 
-const EmployeeRow = ({profile}) => {
+const EmployeeRow = ({ profile }) => {
   const dob = new Date(profile.dob.date);
   const day = dob.getDate();
   const month = dob.getMonth() + 1;
@@ -8,18 +9,19 @@ const EmployeeRow = ({profile}) => {
   const dobDisplay = `${month}-${day}-${year}`;
 
   return (
-    <div className="row">
-      <div className="col-md-1"></div>
-      <div className="col-md-2">
-        <img src={profile.picture.thumbnail} alt="profile" />
+    <div>
+      <hr></hr>
+      <div className="row" id="profileRow">
+        <div className="col-md-2">
+          <img src={profile.picture.thumbnail} alt="profile" />
+        </div>
+        <div className="col-md-2">
+          {profile.name.first + " " + profile.name.last}
+        </div>
+        <div className="col-md-2">{profile.phone}</div>
+        <div className="col-md-4">{profile.email}</div>
+        <div className="col-md-2">{dobDisplay}</div>
       </div>
-      <div className="col-md-2">
-        {profile.name.first + " " + profile.name.last}
-      </div>
-      <div className="col-md-2">{profile.phone}</div>
-      <div className="col-md-2">{profile.email}</div>
-      <div className="col-md-2">{dobDisplay}</div>
-      <div className="col-md-1"></div>
     </div>
   );
 };
